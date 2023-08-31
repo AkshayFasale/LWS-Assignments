31c1e4503a2fbfbf9e8a6fbb5bf139632a3e3184
AkshayFasale <fasaleakshay25@gmail.com>
2023-08-31 23:55:14 +0530

    To find OS Assingment

--- Shell Scripting Assignments/Assignments/osSystem.sh
+++ Shell Scripting Assignments/Assignments/osSystem.sh
@@ -16,5 +16,13 @@ esac
 "
 
 # Returns information about the type of OS running on the machine, eg.
-echo $OSTYPE
-read -p "OS type is $OSTYPE"
+#echo $OSTYPE
+#read -p "OS type is $OSTYPE"
+
+os_name=$(uname -s) # -s   Write the name of the operating system implementation to standard output.
+os_release=$(uname -r) #-r      Write the current release level of the operating system to standard output.
+
+echo "Operating system Name is $os_name"
+echo "Kernel Release is $os_release"
+
+# use man uname to get more information
