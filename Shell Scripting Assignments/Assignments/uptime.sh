d7d2890ae6d945bf9bb984da3fda20619014b4e4
AkshayFasale <fasaleakshay25@gmail.com>
2023-09-01 15:47:29 +0530

    Uptime assignment

--- Shell Scripting Assignments/Assignments/uptime.sh
+++ Shell Scripting Assignments/Assignments/uptime.sh
@@ -2,4 +2,13 @@
 
 #Shell script to restart the system if instance has been up for 2 weeks
 
-echo $uptime
+uptime_seconds=$(cat /proc/uptime | awk -F'.' 'print $1')
+
+max_uptime = 1209600
+
+if [ "$uptime_seconds" -gt "$max_uptime" ]; then
+    echo "System has been up for more than 2 weeks. Restarting..."
+    sudo reboot
+else
+    echo "System uptime is within range."
+fi
